00947ad99fb1fd85e240cad0edbfed59c4c42cb5
IsracardTeam <33055665+IsracardTeam@users.noreply.github.com>
2018-06-13 09:56:04 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import {Animated, StyleSheet, View, Dimensions} from 'react-native';
+import {Animated, ScrollView, StyleSheet, View, Dimensions, Text, PanResponder} from 'react-native';
 
 
 const {width} = Dimensions.get('window')
@@ -20,45 +20,56 @@ class CollapsingHeader extends Component {
         }
     }
 
-    render() {
+    onScroll = (event) => {
 
+    }
+
+
+    render() {
         const headerPosition = this.state.scrollY.interpolate({
             inputRange: [0, this.HEADER_SCROLL_DISTANCE],
             outputRange: [0, - this.HEADER_SCROLL_DISTANCE],
-            extrapolate: 'clamp' // clamp so translateY can’t go beyond -1199
+            extrapolate: 'clamp' // clamp so translateY can’t go beyond -160
+        })
+
+        const paddingSize = this.state.scrollY.interpolate({
+            inputRange: [0, this.HEADER_SCROLL_DISTANCE],
+            outputRange: [this.props.headerMaxHeight, this.props.headerMinHeight],
+            extrapolate: 'clamp' // clamp so translateY can’t go beyond -160
         })
 
         const opacity = this.state.scrollY.interpolate({
-            inputRange: [0, HEADER_SCROLL_DISTANCE],
+            inputRange: [0, this.HEADER_SCROLL_DISTANCE],
             outputRange: [1, 0],
-            extrapolate: 'clamp'
+            extrapolate: 'clamp' // clamp so translateY can’t go beyond -160
         })
 
         return (
-            <View style={styles.container}>
-                <Animated.View style={[styles.header,
+            <Animated.View style={[styles.container,{paddingTop: paddingSize}]}>
+                <Animated.View style={[styles.header,{height: this.props.headerMaxHeight},
                     {transform: [{
                         translateY: headerPosition
                     }]
                 }]} >
-                    {this.props.header}
+                    {this.props.header(opacity)}
                 </Animated.View>
 
-                <Animated.ScrollView
-                    contentContainerStyle={styles.contentContainer} 
-                    scrollEventThrottle={this.props.scrollEventThrottle}
-                    bounces={false}
-                    style={{zIndex: -1}}
-                    onScroll={Animated.event(
-                        [{ nativeEvent: { contentOffset: { y: this.state.scrollY } } }],
-                        {
-                          useNativeDriver: true
-                        },
-                      )}
-                >
-                    {this.props.children}
-                </Animated.ScrollView>
-            </View>)
+                    <Animated.ScrollView
+                        //contentContainerStyle={[{paddingTop: this.props.headerMaxHeight},this.props.contentContainerStyle]} 
+                        scrollEventThrottle={this.props.scrollEventThrottle}
+                        bounces={false}
+                        style={{zIndex: -1}}
+                        onScroll={Animated.event(
+                            [{ nativeEvent: { contentOffset: { y: this.state.scrollY } } }],
+                            {
+                            useNativeDriver: false,
+                            listener: this.onScroll
+                            },
+                        )}
+                    >
+                        {this.props.children}
+                    </Animated.ScrollView>
+            </Animated.View>)
     }
 }
 
@@ -76,6 +87,13 @@ const styles = StyleSheet.create({
         right: 0,
         height: HEADER_MAX_HEIGHT,
         width,
+        shadowColor: "rgba(1, 12, 39, 0.2)",
+        shadowOffset: {
+          width: 0,
+          height: 0
+        },
+        shadowRadius: 4,
+        shadowOpacity: 1
     },
     contentContainer: {
         width,
